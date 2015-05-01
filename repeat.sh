b0614e26fb53ddb45dc3a8309036499d0563d967
E. Choroba <choroba@matfyz.cz>
2015-05-01 22:42:30 +0200

    repeat script added

---
+++ repeat.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# repeat.sh N command [arguments]
+# Repeat the command N times, report the frequency of exit statuses.
+
+times=$1
+shift
+
+declare -A status
+for i in $(seq 1 "$times") ; do
+    "$@"
+    (( status[$?]++ ))
+done
+
+for code in "${!status[@]}" ; do
+    echo "$code" "${status[$code]}x"
+done | sort -nk2
+
