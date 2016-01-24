32623e760e2d60fa666dba0189715fe576ddf1ff
E. Choroba <choroba@matfyz.cz>
2016-01-25 00:03:57 +0100

    Better htmlize
    - start emacs if can't connect
    - process more files at once
    - skip existing new files (like a Makefile)

--- htmlize.sh
+++ htmlize.sh
@@ -1,8 +1,34 @@
 #! /bin/bash
+set -eu
+
+server_file=/tmp/emacs$UID/server
+
+new_emacs=0
+if ! emacsclient --eval 't' &> /dev/null ; then
+    echo Starting emacs... >&2
+    emacs --eval '(server-mode)' &> /dev/null &
+    i=0
+    until emacsclient --eval 't' &> /dev/null ; do
+        sleep .1
+        ((++i > 20)) && break
+    done
+    new_emacs=1
+    [[ -e $server_file ]]
+fi
+
 for file ; do
     if [[ -f $file ]] ; then
-        /usr/bin/emacs -nw --eval '(progn (setq kill-emacs-hook nil)(htmlize-file "'"$file"'") (kill-emacs))' > /dev/null
+        if [[ $file -nt $file.html ]] ; then
+            emacsclient --eval '(htmlize-file "'"$file"'")' &> /dev/null
+            [[ $file.html -nt $file ]]
+        else
+            echo "$file.html" already exists >&2
+        fi
     else
         echo "$file" not found, skipping... >&2
     fi
 done
+
+if (( new_emacs )) ; then
+    emacsclient --eval '(kill-emacs)'
+fi
