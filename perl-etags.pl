6cf666970193f8f009b6b1485634f57f8a01aa60
E. Choroba <choroba@matfyz.cz>
2016-04-05 21:47:18 +0200

    Support "method", testing script added
    + debug warning removed

--- perl-etags.pl
+++ perl-etags.pl
@@ -15,10 +15,10 @@ sub wanted {
     my $code = $path->slurp;
     my %found;
     for my $regex (
-                   qr/^\s* ((sub) \s+ (\w+)) /xm,
-                   qr/^\s* ((alias) \s+ (\w+)) /xm,
-                   qr/^\s* ((has) \s+ (?:["']\+?)? (\w+)) /xm,
-                   qr/^\s* ((has) \s+ \[ \s* qw \s* . \s* ([^)\]\/>]+)) /xm,
+                   qr/^(\s* (sub | method) \s+ (\w+)) /xm,
+                   qr/^(\s* (alias) \s+ (\w+)) /xm,
+                   qr/^(\s* (has) \s+ (?:["']\+?)? (\w+)) /xm,
+                   qr/^(\s* (has) \s+ \[ \s* qw \s* . \s* ([^)\]\/>]+)) /xm,
                   ) {
         while ($code =~ /$regex/g) {
             my ($full, $keyword, $funcs) = ($1, $2, $3);
@@ -26,7 +26,6 @@ sub wanted {
                 my $pos = pos($code) - length $full;
                 my $nl_count = substr($code, 0, $pos) =~ tr/\n//;
                 my ($single_line) = $full =~ /(.*\Q$func\E.*)/g;
-                warn "$full\n\t[$single_line]";
                 $found{$keyword}{$func} = [$nl_count + 1, $pos, $single_line];
             }
         }
@@ -73,3 +72,40 @@ tries to handle C<Moose> keywords as well (C<has>, C<alias>).
 
 =cut
 
+__END__
+
+# Tested on the following:
+# ------------------------
+
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+package Etags::Test;
+use Moose;
+use MooseX::Aliases;
+
+has bare => (is => 'rw');
+
+has 'quoted' => (is => 'rw');
+
+has "double_quoted" => (is => 'rw');
+
+has [qw[ list1 list2 ]] => (is => 'ro');
+
+has [qw[ multi
+         line
+      ]] => (is => 'ro');
+
+sub method {}
+
+sub
+method2 {}
+
+alias m2 => 'method2';
+
+use MooseX::Declare;
+
+class Etags::Test2 {
+    method method3 (Num $count) {}
+}
