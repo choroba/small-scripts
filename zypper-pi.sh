6c7737d82b29db850ae540adafc4238b4078a411
E. Choroba <choroba@matfyz.cz>
2020-07-29 08:58:30 +0200

    Include blocked updates

--- zypper-pi.sh
+++ zypper-pi.sh
@@ -5,7 +5,8 @@ grep -q '<update ' "$tmp" && xsh <<EOF | less
   quiet ;
   open "$tmp" ;
   for &{ sort :n :k (substring(@name, 10, 4) * 10000 + substring(@name, 15))
-              /stream/update-status/update-list/update }
+              ( /stream/update-status/update-list/update
+              | /stream/update-status/blocked-update-list/update ) }
       echo @name {"\n"} (summary) {"\n"} (description) {"\n"} {"-" x 76} ;
 EOF
 rm "$tmp"
