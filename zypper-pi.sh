c7640e04b0d9eb69eaf6c8892c57cd6c7d7ac13c
E. Choroba <choroba@matfyz.cz>
2022-04-25 20:34:26 +0200

    Fix sorting in 15.3
    Packages come with different prefixes

--- zypper-pi.sh
+++ zypper-pi.sh
@@ -4,7 +4,9 @@ zypper -x lp > "$tmp"
 grep -q '<update ' "$tmp" && xsh <<EOF | less
   quiet ;
   open "$tmp" ;
-  for &{ sort :n :k (substring(@name, 19, 4) * 10000 + substring(@name, 24))
+  for &{ sort :n :k (
+      xsh:match(@name, 'openSUSE(?:-SLE-15\.3)?-([0-9]{4})-') * 10000
+      + xsh:match(@name, 'openSUSE(?:-SLE-15\.3)?-[0-9]{4}-([0-9]+)'))
               ( /stream/update-status/update-list/update
               | /stream/update-status/blocked-update-list/update ) }
       echo @name {"\n"} (summary) {"\n"} (description) {"\n"} {"-" x 76} ;
