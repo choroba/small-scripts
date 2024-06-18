30f5097f821deb15fb45079eb2d2f24b9b43ef43
E. Choroba <choroba@matfyz.cz>
2024-06-18 08:04:34 +0200

    Zoom uses a full month name, not a short one
    which wasn't apparent in May.

--- get-zoom-latest.pl
+++ get-zoom-latest.pl
@@ -13,7 +13,7 @@ my $fm = 'Firefox::Marionette'->new->go(
 $fm->await(sub { $fm->find_class('style-two') });
 for my $h3 ($fm->find_tag('h3')) {
     if ($h3->text =~ /^(\w+ \d+, \d+) (version [0-9.]+ )\([0-9]+\)/) {
-        my $release_date = 'Time::Piece'->strptime($1, '%b %d, %Y');
+        my $release_date = 'Time::Piece'->strptime($1, '%B %d, %Y');
         if ($release_date <= localtime) {
             say $2;
             last
