153341b8f91003a6e8fb0781e22ef1226b98d3f9
E. Choroba <choroba@matfyz.cz>
2024-07-19 21:17:05 +0200

    Wait for the strong element instead of style-two class
    The class is no longer present in the zoom release notes page.

--- get-zoom-latest.pl
+++ get-zoom-latest.pl
@@ -10,7 +10,7 @@ my $fm = 'Firefox::Marionette'->new->go(
     'https://support.zoom.com/hc/en/article?id=zm_kb&sysparm_article=KB0068973'
 );
 
-$fm->await(sub { $fm->find_class('style-two') });
+$fm->await(sub { $fm->find_tag('strong') });
 for my $h3 ($fm->find_tag('h3')) {
     if ($h3->text =~ /^(\w+ \d+, \d+) (version [0-9.]+ )\([0-9]+\)/) {
         my $release_date = 'Time::Piece'->strptime($1, '%B %d, %Y');
