fb73132f98f9ecb0fa7111abc445c0be036a3ab5
E. Choroba <choroba@matfyz.cz>
2024-09-20 10:12:14 +0200

    Use the download page instead of the release notes
    The release notes page contains "--" as the current released Linux
    version.

--- get-zoom-latest.pl
+++ get-zoom-latest.pl
@@ -6,36 +6,18 @@ use feature qw{ say };
 use Firefox::Marionette;
 
 my $fm = 'Firefox::Marionette'->new->go(
-    'https://support.zoom.com/hc/en/article?id=zm_kb&sysparm_article=KB0061222'
+    'https://zoom.us/download?os=linux'
 );
 
-$fm->await(sub { $fm->find_tag('article') });
+$fm->await(sub { $fm->find_tag('h2') });
 
 my $stage = "START";
 my $column;
-for my $element ($fm->find('(//h2 | //th | //td)')) {
-    if ('START' eq $stage
-        && 'h2' eq $element->tag_name
-        && 'Released' eq $element->text
-    ) {
-        $stage = 'RELEASED';
-
-    } elsif ('RELEASED' eq $stage
-             && 'th' eq $element->tag_name
-             && 'Linux' eq $element->text
-    ) {
-        $column = $element->attribute('data-col');
-        $stage = 'TABLE';
-
-    } elsif ('TABLE' eq $stage
-             && 'td' eq $element->tag_name
-             && 0 == $column--
-    ) {
-        my $version = $element->text;
-        say $version =~ s/version|\(.*//gr;
-        exit
-    }
+for my $element ($fm->find_class('version-detail')) {
+    my $version = $element->text;
+    say $version =~ s/Version |\(.*//gr;
+    exit
 }
-die 'Linux not found.';
+die 'Not found';
 
 # Fetch the latest zoom client version from the Angular heavy zoom page.
